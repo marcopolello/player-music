add89c28dbb882fff35b0eac3bf2c89daeee096f
Marco Polello <mpolello@alloy.it>
2023-03-17 15:37:05 +0100

    init

---
+++ src/components/Nav.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMusic, faCompactDisc } from "@fortawesome/free-solid-svg-icons";
+import { FaYoutube } from 'react-icons/fa';
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaSpotify } from "react-icons/fa";
+
+const Nav = ({ libraryStatus, setLibraryStatus }) => {
+  return (
+    <nav>
+      <h1>
+        Straahl <FontAwesomeIcon icon={faCompactDisc} /> Player 
+      </h1>
+
+      <div>
+        <a href={'https://open.spotify.com/artist/4UP42TYGukrykuY5FZPjC9'}>< FaSpotify size={20} style={{color: 'rgb(65, 65, 65)'}}/></a>
+        <a href={'https://www.youtube.com/@abyond6583/videos'}>< FaYoutube size={20} style={{color: 'rgb(65, 65, 65)'}}/></a>  
+        <a href={'https://www.instagram.com/straahllll/'}>< FaInstagram size={20} style={{color: 'rgb(65, 65, 65)'}}/></a>   
+        <a href={'https://www.facebook.com/Straahl'}>< FaFacebook size={20} style={{color: 'rgb(65, 65, 65)'}}/></a>
+      </div>
+
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+        Library <FontAwesomeIcon icon={faMusic} />
+      </button>
+
+    </nav>
+  );
+};
+
+export default Nav;
+
---
+++ src/playlist.js
@@ -0,0 +1,79 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Ancient live",
+      cover:
+        "https://yt3.googleusercontent.com/hJkajh-mv9_8_BNU7nGOd733SOQdzNrczXecFxp-X1xsBPbb6OwtGZNRUU_3-PpZPzfcYSXkDA=s176-c-k-c0x00ffffff-no-rj",
+      artist: "Straahl",
+      audio: "https://drive.google.com/uc?id=1Oox-y3hyWZB2WORCq6OKq1WN1lwqMLv4&authuser=0&export=download",
+      color: ["#47609D", "#E88774"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Onde live",
+      cover:
+        "https://yt3.googleusercontent.com/hJkajh-mv9_8_BNU7nGOd733SOQdzNrczXecFxp-X1xsBPbb6OwtGZNRUU_3-PpZPzfcYSXkDA=s176-c-k-c0x00ffffff-no-rj",
+      artist: "Straahl",
+      audio: "https://drive.google.com/uc?id=1rMh0tG1PapJQG6wJjPWykuFuC5wWpxjI&authuser=0&export=download",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "FIRST live studio",
+      cover:
+        "https://yt3.googleusercontent.com/hJkajh-mv9_8_BNU7nGOd733SOQdzNrczXecFxp-X1xsBPbb6OwtGZNRUU_3-PpZPzfcYSXkDA=s176-c-k-c0x00ffffff-no-rj",
+      artist: "Straahl",
+      audio: "https://drive.google.com/uc?id=1oZ3rLpPE6RLbTgf7Sq_KQt1TdZF1YXXo&authuser=0&export=download",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Robotik",
+      cover:
+        "https://i.scdn.co/image/ab67616d0000b273c2865e5cb72fb6928d5c1c6f",
+      artist: "Straahl",
+      audio: "https://drive.google.com/uc?id=1WIk6PuvsB8oHQT8fsfwaL9SMZEMkb2mE&authuser=0&export=download",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Alex Sanderson",
+      cover:
+        "https://i.scdn.co/image/ab67616d0000b2737936c4f8fb9ed60a6a9931ee",
+      artist: "Straahl",
+      audio: "https://drive.google.com/uc?id=198_B8IditQ_Kq9vgshDtSUPymjqYyWcp&authuser=0&export=download",
+      color: ["#335561", "#3A393E"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Floating Fear",
+      cover:
+        "https://i.scdn.co/image/ab67616d0000b2737936c4f8fb9ed60a6a9931ee",
+      artist: "Misha, Jussi Halme",
+      audio: "https://drive.google.com/uc?id=1cPXXdv9bRivM-W1xsturl4ihpbIooO9f&authuser=0&export=download",
+      color: ["#2A416D", "#E98087"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Meanwhile",
+      cover:
+        "https://i.scdn.co/image/ab67616d0000b2737936c4f8fb9ed60a6a9931ee",
+      artist: "Straahl",
+      audio: "https://drive.google.com/uc?id=1P8zNtS4o_eHJpHe0NJ7nH8o5NtXUsJ30&authuser=0&export=download",
+      color: ["#BD3D76", "#551853"],
+      id: uuidv4(),
+      active: false,
+    },
+
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
